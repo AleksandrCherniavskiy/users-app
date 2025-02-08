cb644a78d93e36ad298de63052085694a19aa857
Alex Cherniavskyi <aleksandr.cherniavskyi28@gmail.com>
2025-02-08 19:17:37 +0200

    add routing for users page and create users page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: 'users',
+  loadComponent: () => import('./users-page/users-page.component').then(m => m.UsersPageComponent),
+}, {
+  path: '**',
+  redirectTo: 'users',
+}];
