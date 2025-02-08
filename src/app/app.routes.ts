d2bdc927d4e45c66405bfab112d6944b663726e8
Alex Cherniavskyi <aleksandr.cherniavskyi28@gmail.com>
2025-02-08 19:33:59 +0200

    add ESLint and Prettier configuration, update styles and formatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [{
-  path: 'users',
-  loadComponent: () => import('./users-page/users-page.component').then(m => m.UsersPageComponent),
-}, {
-  path: '**',
-  redirectTo: 'users',
-}];
+export const routes: Routes = [
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./users-page/users-page.component').then(
+        (m) => m.UsersPageComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'users',
+  },
+];
--- src/app/users-page/users-page.component.ts
+++ src/app/users-page/users-page.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './users-page.component.html',
 })
-export class UsersPageComponent {
-
-}
+export class UsersPageComponent {}
